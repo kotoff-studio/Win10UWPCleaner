027c2a446b6991779ef40dfa2460659b3c77ec92
kotoff-studio <76766132+kotoff-studio@users.noreply.github.com>
2021-05-11 17:21:43 +0300

    Create main.ps1

---
+++ main.ps1
@@ -0,0 +1,131 @@
+3D Builder
+Get-AppxPackage *3dbuilder* | Remove-AppxPackage
+
+Alarms & Clock
+Get-AppxPackage *WindowsAlarms* | Remove-AppxPackage
+
+App Connector
+Get-AppxPackage *Appconnector* | Remove-AppxPackage
+
+Asphalt 8:Airborne
+Get-AppxPackage *Asphalt8Airborne* | Remove-AppxPackage
+
+Calculator
+Get-AppxPackage *WindowsCalculator* | Remove-AppxPackage
+
+Calendar and Mail
+Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage
+
+Camera
+Get-AppxPackage *WindowsCamera* | Remove-AppxPackage
+
+Candy Crush Soda Saga
+Get-AppxPackage *CandyCrushSodaSaga* | Remove-AppxPackage
+
+Drawboard PDF
+Get-AppxPackage *DrawboardPDF* | Remove-AppxPackage
+
+Facebook
+Get-AppxPackage *Facebook* | Remove-AppxPackage
+
+Farmville 2:Country Escape
+Get-AppxPackage *FarmVille2CountryEscape* | Remove-AppxPackage
+
+Feedback Hub
+Get-AppxPackage *WindowsFeedbackHub* | Remove-AppxPackage
+
+Get Office
+Get-AppxPackage *officehub* | Remove-AppxPackage
+
+Get Skype
+Get-AppxPackage *Microsoft.SkypeApp* | Remove-AppxPackage
+
+Get Started Tips
+Get-AppxPackage *Getstarted* | Remove-AppxPackage
+
+Groove Music
+Get-AppxPackage *ZuneMusic* | Remove-AppxPackage
+
+Mail Calendar
+Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage
+
+Maps
+Get-AppxPackage *WindowsMaps* | Remove-AppxPackage
+
+Messaging + Skype
+Get-AppxPackage *Messaging* | Remove-AppxPackage
+
+Microsoft Solitaire Collection
+Get-AppxPackage *MicrosoftSolitaireCollection* | Remove-AppxPackage
+
+Microsoft Wallet
+Get-AppxPackage *Wallet* | Remove-AppxPackage
+
+Microsoft Wi-Fi
+Get-AppxPackage *ConnectivityStore* | Remove-AppxPackage
+
+MinecraftUWP
+Get-AppxPackage *MinecraftUWP* | Remove-AppxPackage
+
+Money
+Get-AppxPackage *bingfinance* | Remove-AppxPackage
+
+Movies & TV
+Get-AppxPackage *ZuneVideo* | Remove-AppxPackage
+
+Netflix
+Get-AppxPackage *Netflix* | Remove-AppxPackage
+
+News
+Get-AppxPackage *BingNews* | Remove-AppxPackage
+
+OneNote
+Get-AppxPackage *OneNote* | Remove-AppxPackage
+
+Paid Wi-Fi & Cellular
+Get-AppxPackage *OneConnect* | Remove-AppxPackage
+
+Paint 3D
+Get-AppxPackage *MSPaint* | Remove-AppxPackage
+
+Pandora
+Get-AppxPackage *PandoraMediaInc* | Remove-AppxPackage
+
+People
+Get-AppxPackage *People* | Remove-AppxPackage
+
+Phone
+Get-AppxPackage *CommsPhone* | Remove-AppxPackage
+
+Phone Companion
+Get-AppxPackage *windowsphone* | Remove-AppxPackage
+
+Photos
+Get-AppxPackage *Photos* | Remove-AppxPackage
+
+Get-AppxPackage *WindowsScan* | Remove-AppxPackage
+
+Get-AppxPackage *SkypeApp* | Remove-AppxPackage
+
+Get-AppxPackage *bingsports* | Remove-AppxPackage
+
+Get-AppxPackage *MicrosoftStickyNotes* | Remove-AppxPackage
+
+Get-AppxPackage *WindowsStore* | Remove-AppxPackage
+
+Get-AppxPackage *Office.Sway* | Remove-AppxPackage
+
+Get-AppxPackage *Getstarted* | Remove-AppxPackage
+
+Get-AppxPackage *Twitter* | Remove-AppxPackage
+
+Get-AppxPackage *Microsoft3DViewer* | Remove-AppxPackage
+
+Voice Recorder
+Get-AppxPackage *soundrecorder* | Remove-AppxPackage
+
+Weather
+Get-AppxPackage *bingweather* | Remove-AppxPackage
+Get-AppxPackage *XboxApp* | Remove-AppxPackage
+Get-AppxPackage *XboxOneSmartGlass* | Remove-AppxPackage
+Get-AppxPackage *XboxSpeechToTextOverlay* | Remove-AppxPackag
